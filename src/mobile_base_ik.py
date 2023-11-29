f1d596869616a27c602c0853780c35f16f8da3ee
Wenqi Ding <wenqi22@mit.edu>
2023-11-29 02:25:46 +0000

    init make hardwarestation

--- src/mobile_base_ik.py
+++ src/mobile_base_ik.py
@@ -18,12 +18,109 @@ from pydrake.all import (
     Role,
     eq,
     RigidTransform,
+    InverseDynamicsController,
 )
 from manipulation import ConfigureParser, running_as_notebook
+# from manipulation.scenarios import AddMultibodyTriad, MakeManipulationStation
+from manipulation.station import MakeHardwareStation, load_scenario
+
 import pydot
 from IPython.display import display, Image, SVG
-
 import math
+
+def setup_hardware_station(meshcat):
+    scenario_data = """
+directives:
+- add_directives:
+    file: file:///workspaces/final_project/drake_obstacles_nopkg.yaml
+
+"""
+    scenario = load_scenario(data=scenario_data)
+    station = MakeHardwareStation(scenario, meshcat=meshcat)
+    context = station.CreateDefaultContext()
+
+    # station.GetInputPort("wsg.position").FixValue(context, [0.1])
+    station.ForcedPublish(context)
+   
+    plant = station.GetSubsystemByName("plant")
+
+    return station, plant
+
+def setup_manipulation_station(meshcat):
+    
+    model_directives = """
+directives:
+- add_model:
+    name: ground
+    file: file:///workspaces/final_project/objects/ground.sdf
+- add_weld:
+    parent: world
+    child: ground::base
+    X_PC:
+        translation: [0, 0, 0]
+
+- add_model:
+    name: pr2
+    # file: file:///workspaces/final_project/objects/pr2_simplified.urdf
+    file: package://manipulation/pr2_collision_fixed.urdf
+- add_model:
+    name: rock2
+    file: file:///workspaces/final_project/objects/Cliff_Rock_One_OBJ.sdf
+- add_weld:
+    parent: world
+    child: rock2::Cliff_Rock_One_OBJ
+    X_PC:
+        translation: [-1, -1, 0]
+
+- add_model:
+    name: mountain1
+    file: file:///workspaces/final_project/objects/mountain_OBJ.sdf
+- add_weld:
+    parent: world
+    child: mountain1::mountain_OBJ
+    X_PC:
+        translation: [1, 1, 0]       
+"""
+
+    builder = DiagramBuilder()
+    station = builder.AddSystem(
+        MakeHardwareStation(
+            # filename="file:///workspaces/final_project/drake_obstacles.dmd.yaml",
+            # filename="package://moon/drake_obstacles.dmd.yaml",
+            # time_step=1e-3,
+            model_directives=model_directives,
+        )
+    )
+    plant = station.GetSubsystemByName("plant")
+    scene_graph = station.GetSubsystemByName("scene_graph")
+    AddMultibodyTriad(plant.GetFrameByName("body"), scene_graph)
+
+    MeshcatVisualizer.AddToBuilder(
+        builder,
+        station.GetOutputPort("query_object"),
+        meshcat,
+        MeshcatVisualizerParams(delete_on_initialization_event=False),
+    )
+
+    wsg_position = builder.AddSystem(ConstantVectorSource([0.1]))
+    builder.Connect(
+        wsg_position.get_output_port(), station.GetInputPort("wsg_position")
+    )
+
+    diagram = builder.Build()
+
+    context = plant.CreateDefaultContext()
+    gripper = plant.GetBodyByName("body")
+
+    initial_pose = plant.EvalBodyPoseInWorld(context, gripper)
+
+    simulator = Simulator(diagram)
+    simulator.set_target_realtime_rate(1.0)
+    simulator.AdvanceTo(0.01)
+    print("done simulator build")
+
+    return initial_pose
+
 
 def build_env(meshcat):
     """Load in models and build the diagram."""
@@ -200,20 +297,33 @@ def solve_ik(meshcat, X_WG, max_tries=10, fix_base=False, base_pose=np.zeros(3))
     return None
 
 def set_position(meshcat, X_WG, max_tries=10, fix_base=False, base_pose=np.zeros(3)):
-    diagram, plant, scene_graph = build_env(meshcat)
+    # diagram, plant, scene_graph = build_env(meshcat)
 
+    # using hardware station
+    plant, station = setup_hardware_station(meshcat)
+    # initialize context
+    station_context = station.CreateDefaultContext()
+    plant_context = plant.GetMyContextFromRoot(station_context)
+
+    # previous code 
     world_frame = plant.world_frame()
     gripper_frame = plant.GetFrameByName("l_gripper_palm_link")
 
     context = diagram.CreateDefaultContext()
     plant_context = plant.GetMyContextFromRoot(context)
-    print('1', plant_context)
     sg_context = scene_graph.GetMyContextFromRoot(context)
     filterCollsionGeometry(scene_graph, sg_context)
 
+    ik = InverseKinematics(plant, plant_context)
+    q_variables = ik.q()  # Get variables for MathematicalProgram
+    q_len = len(q_variables)
+    print("q_var len: ", q_len)
+    # q_nominal = np.zeros(len(q_variables))
+
     goal_position = X_WG.translation()
-    q_variables = np.concatenate((np.array(goal_position), np.zeros(15)))
-    print(q_variables)
+    q_variables = np.concatenate((np.array(goal_position), np.zeros(q_len - 3)))
+    # q_variables = q_nominal
+    print("q_varibles: " , q_variables)
 
 
     if running_as_notebook:
